0c031753d0b79020e4e34d3c343095957d8dc98e
Nehasharma97549 <nehash97549@gmail.com>
2025-06-15 12:02:37 +0530

    add files 1.0

--- project10.js
+++ project10.js
@@ -1,7 +1,25 @@
-// Function to update the text color based on the slider value
+/*Function to update the text color based on the slider value
 document.getElementById('color-slider').addEventListener('input', function(event) {
     const sliderValue = event.target.value;
     
     // Update the text color dynamically (range from 0 to 255)
     document.getElementById('text-display').style.color = `rgb(${sliderValue}, 0, 255 - ${sliderValue})`;
+});*/
+
+
+
+function changeColor(hue){
+    let h = document.getElementById('displayText');
+    h.style.color = `hsl(${hue}, 100%, 50%)`; // Use backticks (``) for string interpolation
+}
+let slider=document.getElementById('colorSlider');
+slider.addEventListener('input',function(){
+    updatecolorGradient(this.value)
 });
+
+
+function updatecolorGradient(hue){
+        const colorGradient=document.getElementById('colorGradient');
+        colorGradient.style.background=`linear-gradient(to right,hsl(0,100%,50%),hsl(${hue},100%,50%))`;
+
+}
